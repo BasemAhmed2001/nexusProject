fca70261e72aed501f2e5177fcc430ccc0cd7bf6
basemAhmed2001 <basem522001@gmail.com>
2026-03-09 03:54:42 +0200

    add database

--- src/main/java/com/project/backend/controllers/AuthorController.java
+++ src/main/java/com/project/backend/controllers/AuthorController.java
@@ -5,6 +5,10 @@ import com.project.backend.entities.Author;
 import com.project.backend.entities.Paper;
 import com.project.backend.repositories.AuthorRepository;
 import com.project.backend.repositories.PaperRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,6 +34,31 @@ public class AuthorController {
         return authorRepository.findById(id).get();
 
     }
+    @GetMapping("/findByExactName")
+    public List<Author> getAuthorByName(@RequestParam String name) {
+        return authorRepository.findAuthorsByName(name);
+    }
+
+    @GetMapping("/findByNameContainingIGC")
+    public List<Author> getAuthorsByNameContainingIgnoreCase(@RequestParam String name) {
+        return authorRepository.findAuthorsByNameContainingIgnoreCase(name);
+    }
+
+    @GetMapping("/findByNameContainsIGC")
+    public List<Author> getAuthorsByNameContainsIgnoreCase(@RequestParam String name) {
+        return authorRepository.findAuthorsByNameContainsIgnoreCase(name);
+    }
+
+    @GetMapping("/findAll")
+    public Page<Author> getAll(@RequestParam(defaultValue = "0") int page
+                                , @RequestParam(defaultValue = "10") int size) {
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by("name").ascending());
+
+        return authorRepository.findAll(pageable);
+    }
+
+
 
     @GetMapping("/categoryFilter")
     public List<Author> filterByCategory(@RequestParam String category) {
